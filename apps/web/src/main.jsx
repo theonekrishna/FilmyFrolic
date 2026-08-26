8280441e141a4dc7dbfe47f9bddc20b0b9eed590
Krishna <saikr@example.com>
2026-08-26 14:11:03 +0530

    fix: import createRoot from react-dom/client (was react) - fixes blank screen

--- apps/web/src/main.jsx
+++ apps/web/src/main.jsx
@@ -1,4 +1,4 @@
-import { createRoot } from "react";
+import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { GoogleOAuthProvider } from "@react-oauth/google";
